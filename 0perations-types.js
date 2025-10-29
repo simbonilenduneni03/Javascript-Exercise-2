1bd81f969b79d9d7b0079aaabf30f885305ab2e8
simbonilenduneni03 <ndunenisimbonile429@gmail.com>
2025-10-29 15:39:34 +0200

    initial commit

---
+++ 0perations-types.js
@@ -0,0 +1,15 @@
+let number1 = 10;
+let number2 = 5;
+
+let str1 = number1.toString();
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+
+console.log(number1)
+console.log(number2)
+console.log(str1)
+console.log(sum)
+console.log(product)
